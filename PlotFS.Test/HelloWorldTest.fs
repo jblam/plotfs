ec70d2bf2d4c83e60cf70469d32c375a34dc9e90
Jeremy <jeremy.blamey@blameysaunders.com.au>
2018-03-16 19:33:00 +1100

    Created a test project

---
+++ PlotFS.Test/HelloWorldTest.fs
@@ -0,0 +1,17 @@
+﻿namespace PlotFS.Test
+
+open Xunit
+open PlotFS
+
+module HelloWorldTest =
+    let doesMathsWork = 1 = 1
+    [<Fact>]
+    let MathsDoesWork() =
+        Assert.True doesMathsWork
+
+    [<Fact>]
+    let ReferenceDoesWork() =
+        let expected = "test"
+        let actual = Say.hello "test"
+        Assert.Contains(expected, actual)
+        

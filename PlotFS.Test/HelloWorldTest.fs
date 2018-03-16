7d24e0422a1e8b43e6448382cfdde6e2c51011cb
Jeremy <jeremy.blamey@blameysaunders.com.au>
2018-03-16 19:33:43 +1100

    Part-defined the type system

--- PlotFS.Test/HelloWorldTest.fs
+++ PlotFS.Test/HelloWorldTest.fs
@@ -15,3 +15,46 @@ module HelloWorldTest =
         let actual = Say.hello "test"
         Assert.Contains(expected, actual)
         
+    [<Fact>]
+    let CanGetFirstDataPoint() =
+        let axes = {
+                Abcissa = Axis()
+                Ordinate = Axis()
+            }
+        let plot = {
+            Axes = {
+                   Abcissa = Axis()
+                   Ordinate = Axis()
+                   }            
+            Series =
+            [
+                {
+                    Title = "demo"
+                    Data = Points [{ X = X 0.0; Y = Y 0.0}]
+                }
+            ]
+        }
+        let actual = Say.gidday plot
+        Assert.Contains("wut", actual)
+
+    [<Fact>]
+    let CanGetFunctionZero() =
+        let axes = {
+                Abcissa = Axis()
+                Ordinate = Axis()
+            }
+        let plot = {
+            Axes = {
+                   Abcissa = Axis()
+                   Ordinate = Axis()
+                   }            
+            Series =
+            [
+                {
+                    Title = "demo"
+                    Data = Function (fun x -> Y 14.0)
+                }
+            ]
+        }
+        let actual = Say.gidday plot
+        Assert.Contains("wut", actual)
--- plotfs/Library.fs
+++ plotfs/Library.fs
@@ -1,5 +1,39 @@
-namespace plotfs
+namespace PlotFS
+
+type x = X of double
+type y = Y of double
+
+[<Struct>]
+type Point = { X: x; Y: y }
+
+type SeriesData =
+    | Points of seq<Point>
+    | Function of (x -> y)
+
+type Series = {
+    Data : SeriesData
+    Title : string
+    }
+type Axis() = class end
+
+type AxisSet = {
+    Abcissa: Axis
+    Ordinate: Axis
+    }
+
+type Plot = {
+    Axes: AxisSet
+    Series: seq<Series>
+    }
 
 module Say =
     let hello name =
-        printfn "Hello %s" name
+        sprintf "Hello %s" name
+
+    let gidday plot =
+        let s1 = Seq.head plot.Series
+        let t = s1.Title
+        let desc = match s1.Data with
+                    | Points p -> ("first point", Seq.head p)
+                    | Function f -> ("y-intercept", { X = X 0.0; Y = f(X 0.0) })
+        sprintf "%s has %s %A" t <|| desc
